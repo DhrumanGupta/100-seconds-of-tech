1cd47c57ebc9b342e7175a76c6d5e3d4c7004b7e
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-06-17 19:14:56 +0530

    change 404 page meta to be more descriptive

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,10 @@ interface INotFoundProps {}
 const NotFound: FC<INotFoundProps> = (props) => {
   return (
     <div className="absolute right-1/2 translate-x-1/2 bottom-1/2 translate-y-1/2">
-      <MetaDecorator title="Not Found" description="Not found" />
+      <MetaDecorator
+        title="Page Not Found"
+        description="The page you were looking for was not found"
+      />
       <h2 className="font-bold text-center text-4xl lg:text-5xl">404</h2>
       <p className="text-center lg:text-lg">
         This is not the page you are looking for. <Link href="/">Go Home</Link>
